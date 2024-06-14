a33bc011115d8a82f6835701e1db4981879e237e
fuego44 <victorandreum@gmail.com>
2024-06-14 09:24:40 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import Home from "./pages/Home";
 import "./styles/app.scss";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, HashRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename="game_platform/">
-      
+    <HashRouter>
       <Routes>
         <Route path={"/"} element={<Home />} />
-        <Route basename="game_platform/" path={"/game/:id"} element={<Home />} />
+        <Route  path={"/game/:id"} element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
